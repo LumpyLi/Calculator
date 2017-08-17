e13e226fdce7a43790e9ab9390e9d983d915a3b1
李喜歡 <napoleonlgc@gmail.com>
2017-08-17 17:36:44 +0800

    根据视频完成代码

--- ViewController.swift
+++ ViewController.swift
@@ -28,15 +28,23 @@ class ViewController: UIViewController {
         }
     }
     
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text! = String(newValue)
+        }
+    }
+    
     @IBAction func performOperation(_ sender: UIButton) {
         userIsInTheMiddleOfTyping = false
         if let mathematicalSymbol = sender.currentTitle {
             switch mathematicalSymbol {
             case "π":
-                display.text = String(Double.pi)
+                displayValue = Double.pi
             case "√":
-                let operand = Double(display.text!)!
-                display.text = String(sqrt(operand))
+                displayValue = sqrt(displayValue)
             default:
                 break
             }
